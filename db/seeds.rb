b4017fc40958c9a1daf7736312b39ef30c1f7ded
Array <arrayest@qq.com>
2015-03-21 10:42:51 +0800

    update db seed and change some views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Company.create(name: "263网络通信有限公司")
-Department.create(name: "技术部", company_id: 1, parent_id: nil)
-Department.create(name: "运营部", company_id: 1, parent_id: nil)
+d = Department.create(name: "董事会", company_id: 1, parent_id: nil)
+d1 = Department.create(name: "技术部", company_id: 1, parent_id: d.id)
+Department.create(name: "运维部", company_id: 1, parent_id: d1.id)
+Department.create(name: "测试部", company_id: 1, parent_id: d1.id)
+Department.create(name: "运营部", company_id: 1, parent_id: d.id)
+Department.create(name: "市场部", company_id: 1, parent_id: d.id)
+Department.create(name: "销售部", company_id: 1, parent_id: d.id)
+Department.create(name: "财务部", company_id: 1, parent_id: d.id)
 100.times do |i|
 	c = Contact.create(name: "Albert#{i}", company_id: 1, department_id: 1)
   Phone.create(contact_id: c.id, phone_number: (18075640000+i).to_s)
